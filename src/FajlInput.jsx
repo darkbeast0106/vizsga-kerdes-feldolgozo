1efcd7e06be9e5da750e55df6806f3e5785050cb
Budaházi Bence <budahazi.bence@petrik.hu>
2022-10-17 01:48:31 +0200

    Formázás

--- src/FajlInput.jsx
+++ src/FajlInput.jsx
@@ -1,8 +1,15 @@
 function FajlInput(props) {
-    const { fileInputChange } = props;
-    return ( 
-        <input type="file" onChange={(e) => fileInputChange(e.target.files[0])} className="form-control"/>
-     );
+  const { fileInputChange } = props;
+  return (
+    <div className="mt-2">
+      <input
+        placeholder="Letöltött kérdésbank"
+        type="file"
+        onChange={(e) => fileInputChange(e.target.files[0])}
+        className="form-control"
+      />
+    </div>
+  );
 }
 
 export default FajlInput;
